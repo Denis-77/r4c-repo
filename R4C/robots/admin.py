47fc9300b0fcda4120e7560156118794f21ca635
denis <academic666@mail.ru>
2023-09-24 22:35:17 +0200

    robot admin

--- R4C/robots/admin.py
+++ R4C/robots/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Robot
+
+
+@admin.register(Robot)
+class RobotAdmin(admin.ModelAdmin):
+    list_display = 'id', 'serial', 'model', 'version', 'created'
