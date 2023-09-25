39675310ffea1fba34381f96fd2e416087965386
denis <academic666@mail.ru>
2023-09-25 23:29:28 +0200

    Task 3 done

--- R4C/customers/views.py
+++ R4C/customers/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
+from django.core.mail import send_mail
 
-# Create your views here.
+from R4C.settings import EMAIL_HOST_USER
+
+
+def send_mail_to_customers(serial: str, list_customer: tuple):
+    model, version = tuple(serial.split('-'))
+    subject = f'{model}-{version} есть в наличии!'
+    message = (
+        'Добрый день!'
+        f'Недавно вы интересовались нашим роботом модели {model}, версии {version}.'
+        'Этот робот теперь в наличии. Если вам подходит этот вариант - '
+        'пожалуйста, свяжитесь с нами'
+    )
+    from_email = EMAIL_HOST_USER
+
+    # TODO следующую строку закоммитить чтобы началась рассылка
+    list_customer = 'можно вписать свою почту для проверки'
+
+    send_mail(
+        subject=subject,
+        message=message,
+        from_email=from_email,
+        recipient_list=list_customer
+    )
+
--- R4C/robots/models.py
+++ R4C/robots/models.py
@@ -1,4 +1,9 @@
 from django.db import models
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
+from orders.models import Order
+from customers.views import send_mail_to_customers
 
 
 class Robot(models.Model):
@@ -6,3 +11,16 @@ class Robot(models.Model):
     model = models.CharField(max_length=2, blank=False, null=False)
     version = models.CharField(max_length=2, blank=False, null=False)
     created = models.DateTimeField(blank=False, null=False)
+
+
+@receiver(post_save, sender=Robot)
+def my_model_post_save(sender, instance, created, **kwargs):
+    orders = (
+        Order.objects
+        .filter(robot_serial=instance.serial)
+        .select_related('customer')
+    )
+
+    emails = [order.customer.email for order in orders]
+    emails = tuple(emails)
+    send_mail_to_customers(serial=instance.serial, list_customer=emails)
