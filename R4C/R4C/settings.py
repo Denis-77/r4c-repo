39f4f88bed8e868f04310f369ac255f195baf0d7
Denis Pisarev <dzianisngu@gmail.com>
2024-07-15 23:58:19 +0200

    Update settings.py

--- R4C/R4C/settings.py
+++ R4C/R4C/settings.py
@@ -20,10 +20,13 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'mztx@x_-=gfhc9xs@bm58m&@3pc7##opo14zob!(l2tus05+jo'
+SECRET_KEY = os.getenv(
+    'DJANGO_SECRET_KEY',
+    'mztx@x_-=gfhc9xs@bm58m&@3pc7##opo14zob!(l2tus05+jo'
+)
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DJANGO_DEBUG', False) == '1'
 
 ALLOWED_HOSTS = []
 
